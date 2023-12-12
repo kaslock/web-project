7db88b433dca7296255d4e240a4334eb75659ef6
Jang Jaeyong <kaslock@naver.com>
2023-12-12 15:35:36 +0900

    useEffect & RenderPropsPattern test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RenderPropsPattern from "./RenderPropsPattern";
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    console.log(`useEffect: ${Date()}`);
+  }, []);
 
   return (
     <>
@@ -27,7 +32,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p>      
+      <RenderPropsPattern/>
     </>
   )
 }
