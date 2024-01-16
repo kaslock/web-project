924ecaa93c86fb73635d109a0c722b87607c6c26
Jang Jaeyong <kaslock@naver.com>
2024-01-16 15:24:18 +0900

    request scope / provider test

--- backend/src/main/java/com/example/backend/controller/SampleController.java
+++ backend/src/main/java/com/example/backend/controller/SampleController.java
@@ -1,24 +1,41 @@
 package com.example.backend.controller;
 
+import com.example.backend.common.MyLogger;
 import com.example.backend.domain.Sample;
+import com.example.backend.service.LogDemoService;
 import com.example.backend.service.SampleService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class SampleController {
 
     private final SampleService sampleService;
-
-    @Autowired
-    public SampleController(SampleService sampleService) {
-        this.sampleService = sampleService;
-    }
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @PostMapping("/sample/new")
     public Long create(Sample sample) {
         return sampleService.join(sample);
+    }
+
+    @GetMapping("/log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("Controller test");
+        logDemoService.logic("testID");
+
+        return "OK";
     }
 }
